9c294724883fc4962c2a3fdb82e44101b98a3f93
Artur Calderon <86498045+artur-calderon@users.noreply.github.com>
2025-05-17 21:56:38 -0400

    moved run.py outside app folder

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
-# from app import create_app, db
-from . import create_app, db
+from app import create_app, db
+# from . import create_app, db
 from flask_cors import CORS
 import requests
 import os

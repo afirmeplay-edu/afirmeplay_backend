49b7044d98442d7b3832e9c83d12f8208bd7b6d1
Artur Calderon <86498045+artur-calderon@users.noreply.github.com>
2025-05-17 21:43:38 -0400

    procfile chaged

--- app/run.py
+++ app/run.py
@@ -2,12 +2,31 @@ from app import create_app, db
 from flask_cors import CORS
 import requests
 import os
+import threading
 
 app = create_app()
 
 PING_INTERVAL = 14 * 60
-SELF_URL = os.environ.get("SELF_URL", )
+SELF_URL = os.environ.get("SELF_URL","https://innovaplay-backend.onrender.com")
 
+def keep_alive():
+    while True:
+        try:
+            res = requests.get(SELF_URL)
+            print(f"[KEEPALIVE] Status: {res.status_code}")
+        except Exception as e:
+            print(f"[KEEPALIVE ERROR] {e}")
+            
+            
+def start_keep_alive():
+    thread = threading.Thread(target=keep_alive, daemon=True)
+    thread.start()
+    
+@app.route("/ping")
+def ping():
+    return "pong",200
+     
+            
 # para permitir somente o backend CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}}, supports_credentials=True)
 # resources={r"/*": {"origins": "https://innovplay.vercel.app"}}
 CORS(app,  supports_credentials=True)
@@ -16,4 +35,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
+    start_keep_alive()
     app.run(debug=True)

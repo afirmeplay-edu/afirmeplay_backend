dec87abf506502b53da9e7330d84061ffafbcd1f
Artur Calderon <86498045+artur-calderon@users.noreply.github.com>
2025-05-17 21:23:14 -0400

    procfile

--- app/run.py
+++ app/run.py
@@ -1,7 +1,12 @@
 from app import create_app, db
 from flask_cors import CORS
+import requests
+import os
 
 app = create_app()
+
+PING_INTERVAL = 14 * 60
+SELF_URL = os.environ.get("SELF_URL", )
 
 # para permitir somente o backend CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}}, supports_credentials=True)
 # resources={r"/*": {"origins": "https://innovplay.vercel.app"}}

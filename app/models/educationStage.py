e23021edb0c6458fafcf592593e10dc9a1c2625f
C. Rafael Oliveira <crafaelsdo@gmail.com>
2025-07-09 09:52:58 -0300

    fix: Corrige erro gerado pelo tipo do id em EducationStage.

--- app/models/educationStage.py
+++ app/models/educationStage.py
@@ -1,10 +1,12 @@
 from app import db
 import uuid
+from sqlalchemy.dialects.postgresql import UUID
+
 
 class EducationStage(db.Model):
-    __tablename__ = 'education_stage'
+    __tablename__ = "education_stage"
 
-    id = db.Column(db.String, primary_key=True, default=lambda: str(uuid.uuid4()))
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = db.Column(db.String(100))
 
     grades = db.relationship("Grade", backref="education_stage")

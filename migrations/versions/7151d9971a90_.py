af7a5732723e17e6dbed362ef52673626d825989
artur-calderon <artur.calderonart@gmail.com>
2025-12-30 15:43:26 -0300

    migration

---
+++ migrations/versions/7151d9971a90_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 7151d9971a90
+Revises: add_play_tv_tables, add_selected_grades_forms
+Create Date: 2025-12-30 15:41:26.328187
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7151d9971a90'
+down_revision = ('add_play_tv_tables', 'add_selected_grades_forms')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
